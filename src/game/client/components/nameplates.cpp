7f1b66edba1d490c6eaccd73509ba5dcb79ab09d
meloƞ <arbeit.t.lechner@gmail.com>
2023-12-25 00:00:38 +0100

    fixed nameplates - refactored function

--- src/game/client/components/nameplates.cpp
+++ src/game/client/components/nameplates.cpp
@@ -215,14 +215,15 @@ void CNamePlates::RenderNameplatePos(vec2 Position, const CNetObj_PlayerInfo *pP
 				TextRender()->RenderTextContainer(m_aNamePlates[ClientID].m_ClanNameTextContainerIndex, TColor, TOutlineColor, Position.x - m_aNamePlates[ClientID].m_ClanNameTextWidth / 2.0f, YOffset);
 		}
 		//tee skin name
-		if(g_Config.m_ClShowSkinName)
+		if (g_Config.m_ClShowSkinName)
 		{
+			const CGameClient::CClientData& ClientData = m_pClient->m_aClients[pPlayerInfo->m_ClientID];
+			const char* pSkinName = ClientData.m_aSkinName;
+
 			YOffset -= FontSizeClan;
-			char aBuf[128];
-			str_format(aBuf, sizeof(aBuf), "%s", m_pClient->m_aClients[pPlayerInfo->m_ClientID].m_aSkinName);
-			float XOffset = TextRender()->TextWidth(FontSize, aBuf, -1, -1.0f) / 2.0f;
+			float XOffset = TextRender()->TextWidth(FontSize, pSkinName, -1, -1.0f) / 2.0f;
 			TextRender()->TextColor(rgb);
-			TextRender()->Text(Position.x - XOffset, YOffset, FontSize, aBuf, -1.0f);
+			TextRender()->Text(Position.x - XOffset, YOffset, FontSize, pSkinName, -1.0f);
 		}
 
 		if(g_Config.m_ClNameplatesFriendMark && m_pClient->m_aClients[ClientID].m_Friend)
